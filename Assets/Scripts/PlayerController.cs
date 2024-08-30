e23a56239ccd1d4d169ee96520d81d51d65d3d0c
Aiko Mei McCarthy <mccarthyaiko@gmail.com>
2024-08-29 22:39:45 -0400

    Set Up, And Player Walks

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int speed;
+
+    private PlayerControls playerControls;
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+    private void OnEnable()
+    {
+        playerControls.Enable();  
+
+    }
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = playerControls.player.Move.ReadValue<Vector3>().x;
+        float z = playerControls.player.Move.ReadValue<Vector3>().y;
+
+        Debug.Log(x + "," + z);
+
+        movement = new Vector3(x, 0, z).normalized;
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
